c19f3c3abfed3c3b16ceb509201e4223ea95e47a
mirza <mirza.ghulam@indovti.com>
2018-03-25 22:05:49 +0700

    Add project files.

---
+++ DataHelpers/DataResult.cs
@@ -0,0 +1,24 @@
+﻿namespace DataHelpers
+{
+    /// <summary>
+    /// A generic class that holds a value of certain operation and status result class
+    /// which can be used to detect whether certain operation succeeded or not
+    /// </summary>
+    /// <typeparam name="T">Any type</typeparam>
+    public class DataResult<T>
+    {
+        public DataResult(T data, StatusResult status)
+        {
+            Data = data;
+            Status = status;
+        }
+        /// <summary>
+        /// Main value passed as a result
+        /// </summary>
+        public T Data { get; private set; }
+        /// <summary>
+        /// Status of the operation
+        /// </summary>
+        public StatusResult Status { get; private set; }
+    }
+}
---
+++ DataHelpers/StatusResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DataHelpers
+{
+    /// <summary>
+    /// Status Result class that contains a flag and list of errors which indicate whether certain
+    /// operation succeeded or not
+    /// </summary>
+    public class StatusResult
+    {
+        public StatusResult(bool isSuccess, List<string> errors)
+        {
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+        /// <summary>
+        /// A flag that indicates certain operation succeeded or not
+        /// </summary>
+        public bool IsSuccess { get; private set; }
+
+        /// <summary>
+        /// List of errors
+        /// </summary>
+        public List<string> Errors { get; private set; }
+    }
+}
